88efe6854581a40bebc162f7158d743e5533e7d9
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2019-11-22 11:36:12 +0200

    WIP PDF viewer

--- src/config.js
+++ src/config.js
@@ -1,12 +1,25 @@
 import chartIcon from '@plone/volto/icons/world.svg';
 import HiddenWidget from './Widgets/Hidden';
 
+import PDFBlockView from './PDFViewer/BlockView';
+import PDFBlockEdit from './PDFViewer/BlockEdit';
+
 import TableauBlockView from './Tableau/BlockView';
 import TableauBlockEdit from './Tableau/BlockEdit';
 
 export function applyConfig(config) {
   config.widgets.id.blocks = HiddenWidget;
   config.widgets.id.blocks_layout = HiddenWidget;
+
+  config.blocks.blocksConfig.pdf_viewer = {
+    id: 'pdf_viewer',
+    title: 'PDF Viewer',
+    view: PDFBlockView,
+    edit: PDFBlockEdit,
+    icon: chartIcon,
+    group: 'custom_addons',
+  };
+
   return config;
 }
 
