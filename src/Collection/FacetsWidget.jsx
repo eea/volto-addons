56b436ac032a9951366961d018bd95c550f13650
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-25 12:34:13 +0200

    WIP on faceted widget

--- src/Collection/FacetsWidget.jsx
+++ src/Collection/FacetsWidget.jsx
@@ -1,10 +1,36 @@
+import { connect } from 'react-redux';
+import { getIndexValues } from '../actions';
 import React, { Component } from 'react';
 
 class FacetsConfigurationWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    if (this.props.index_name) {
+      this.props.getIndexValues(this.props.index_name);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.index_name !== prevProps.index_name) {
+      this.props.getIndexValues(this.props.index_name);
+    }
+  }
   render() {
     console.log('props in widget', this.props);
-    return 'widget';
+    return <div>Hello</div>;
   }
 }
 
-export default FacetsConfigurationWidget;
+export default connect(
+  (state, props) => {
+    return {
+      filters: (state.index_values && state.index_values.items) || [],
+    };
+  },
+  {
+    getIndexValues,
+  },
+)(FacetsConfigurationWidget);
