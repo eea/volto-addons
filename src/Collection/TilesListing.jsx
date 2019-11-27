b4f2e2cf850493f3aac2931a8c0ca57e77372bac
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2019-11-27 14:59:53 +0200

    Collection tile listing: add description

--- src/Collection/TilesListing.jsx
+++ src/Collection/TilesListing.jsx
@@ -11,34 +11,36 @@ const TilesListing = ({ items }) => {
   }
 
   return (
-    <div style={segmentStyle}>
-      <Item.Group>
-        {items.length
-          ? items.map(item => (
+    <div>
+      <div style={segmentStyle}>
+        <Item.Group>
+          {items.length
+            ? items.map(item => (
               <Item key={item['@id']}>
                 <Item.Content>
                   <Item.Header as='a' href={item.url}>{item.title}</Item.Header>
-                  <Item.Description>Description</Item.Description>
+                  <Item.Description>{item.description}</Item.Description>
                   <Item.Extra>
-                     <span>
-                       Content Type: <span style={categoryStyle}>{item['@type']}</span>
-                     </span>
-                     <span>
-                       Date: <span style={categoryStyle}>
-                         <FormattedDate
-                           value={item.created}
-                           day="2-digit"
-                           month="long"
-                           year="numeric"
-                           />
-                       </span>
-                    </span>
-                  </Item.Extra>
-                </Item.Content>
-              </Item>
-            ))
-          : 'No results'}
+                    <span>
+                      Content Type: <span style={categoryStyle}>{item['@type']}</span>
+                  </span>
+                  <span>
+                    Date: <span style={categoryStyle}>
+                    <FormattedDate
+                      value={item.created}
+                      day="2-digit"
+                      month="long"
+                      year="numeric"
+                      />
+                  </span>
+                </span>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))
+        : 'No results'}
       </Item.Group>
+      </div>
     </div>
   );
 };
