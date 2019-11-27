c129536925588508a5c026e59c388e350ebc6432
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2019-11-27 13:18:17 +0200

    Collection tile listing

--- src/Collection/TilesListing.jsx
+++ src/Collection/TilesListing.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import { FormattedDate } from 'react-intl';
+import { Item } from 'semantic-ui-react'
 
 const TilesListing = ({ items }) => {
+  const categoryStyle = {
+    color: '#252525',
+  }
+  const segmentStyle = {
+    marginBottom: '1rem',
+  }
+
   return (
-    <div>
-      {items.length
-        ? items.map(item => (
-            <div key={item['@id']}>
-              <a href={item.url}>{item.title}</a>
-            </div>
-          ))
-        : 'No results'}
+    <div style={segmentStyle}>
+      <Item.Group>
+        {items.length
+          ? items.map(item => (
+              <Item key={item['@id']}>
+                <Item.Content>
+                  <Item.Header as='a' href={item.url}>{item.title}</Item.Header>
+                  <Item.Description>Description</Item.Description>
+                  <Item.Extra>
+                     <span>
+                       Content Type: <span style={categoryStyle}>{item['@type']}</span>
+                     </span>
+                     <span>
+                       Date: <span style={categoryStyle}>
+                         <FormattedDate
+                           value={item.created}
+                           day="2-digit"
+                           month="long"
+                           year="numeric"
+                           />
+                       </span>
+                    </span>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+            ))
+          : 'No results'}
+      </Item.Group>
     </div>
   );
 };
