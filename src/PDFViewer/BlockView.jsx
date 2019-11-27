e9f1cfbd7ecb469468007117bf01fd66b46d460a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-27 10:48:24 +0200

    Added scroll handler

--- src/PDFViewer/BlockView.jsx
+++ src/PDFViewer/BlockView.jsx
@@ -1,12 +1,5 @@
-/**
- * View image block.
- * @module components/manage/Blocks/Image/View
- */
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import cx from 'classnames';
 import { Icon } from '@plone/volto/components';
 import { settings } from '~/config';
 import { flattenToAppURL } from '@plone/volto/helpers';
@@ -20,104 +13,136 @@ import zoomOutSVG from '@plone/volto/icons/remove.svg';
 import downloadSVG from '@plone/volto/icons/move-down.svg';
 
 const LoadablePDFViewer = Loadable({
-  loader: () => import('mgr-pdf-viewer-react'),
+  loader: () => import('./PDFViewer'),
   loading() {
     return <div>Loading PDF file...</div>;
   },
 });
 
-
 class PDFView extends Component {
   state = {
     scale: 1.0,
     scale_ratio: 100,
-  }
+    currentPage: 1,
+    pageCount: 0,
+  };
 
-  increaseScale = () => this.setState(({ scale, scale_ratio }) => ({
-    scale: scale + 0.1,
-    scale_ratio: scale_ratio + 10
-  }))
-  decreaseScale = () => this.setState(({ scale, scale_ratio }) => ({
-    scale: scale - 0.1,
-    scale_ratio: scale_ratio - 10
-  }))
+  increaseScale = () =>
+    this.setState(({ scale, scale_ratio }) => ({
+      scale: scale + 0.1,
+      scale_ratio: scale_ratio + 10,
+    }));
+  decreaseScale = () =>
+    this.setState(({ scale, scale_ratio }) => ({
+      scale: scale - 0.1,
+      scale_ratio: scale_ratio - 10,
+    }));
 
   componentDidMount() {
-    document.querySelector('.pdf-wrapper')
+    document
+      .querySelector('.pdf-wrapper')
       .addEventListener('wheel', this.handleWheel);
   }
 
   componentWillUnmount() {
-    document.querySelector('.pdf-wrapper')
+    document
+      .querySelector('.pdf-wrapper')
       .removeEventListener('wheel', this.handleWheel);
   }
 
-  handleWheel(event) {
+  handleWheel = event => {
+    let page;
     if (event.deltaY < 0) {
-     console.log('scrolling up');
+      page = Math.max(this.state.currentPage - 1, 1);
+      this.setState({
+        currentPage: page,
+      });
+    } else if (event.deltaY > 0) {
+      page = Math.min(this.state.currentPage + 1, this.state.pageCount);
+      this.setState({
+        currentPage: page,
+      });
     }
-    else if (event.deltaY > 0) {
-     console.log('scrolling down');
-    }
-  }
 
-  render () {
-    const { data, detached } = this.props;
+    event.preventDefault();
+  };
+
+  onDocumentComplete = ({ page, pages }) => {
+    this.setState({
+      currentPage: page,
+      pageCount: pages,
+    });
+  };
+
+  render() {
+    const { data } = this.props;
     const dataUrl =
-     (data.url &&
-       (data.url.includes(settings.apiPath)
-         ? `${flattenToAppURL(data.url)}/@@download/file`
-         : data.url)) ||
-     null;
+      (data.url &&
+        (data.url.includes(settings.apiPath)
+          ? `${flattenToAppURL(data.url)}/@@download/file`
+          : data.url)) ||
+      null;
 
     return (
       <div>
         {data.url && (
-         <>
-           {(() => {
-             return (
-               <div>
-                 <div className="pdf-wrapper">
-                   <h2 className="pdf-title">{data.url.split('/').slice(-1)[0]}</h2>
-                   <div className="pdf-toolbar pdf-toolbar-top">
-                     <div>
-                       <button className="pdf-toolbar-btn" title="Zoom In"
-                         onClick={this.increaseScale}>
-                         <Icon name={zoomInSVG} size="15px" />
-                       </button>
-                       <div className="scale-separator"></div>
-                       <button className="pdf-toolbar-btn" title="Zoom Out"
-                         onClick={this.decreaseScale}>
-                         <Icon name={zoomOutSVG} size="15px" />
-                       </button>
-                       <p className="scale-ratio">{this.state.scale_ratio + '%'}</p>
-                     </div>
-                     <div>
-                       <a href={dataUrl}>
-                         <button className="pdf-toolbar-btn" title="Download">
-                           <Icon name={downloadSVG} size="15px" />
-                         </button>
-                       </a>
-                     </div>
-                   </div>
-                   <p>{this.state.page}</p>
+          <>
+            {(() => {
+              return (
+                <div>
+                  <div className="pdf-wrapper">
+                    <h2 className="pdf-title">
+                      {data.url.split('/').slice(-1)[0]}
+                    </h2>
+                    <div className="pdf-toolbar pdf-toolbar-top">
+                      <div>
+                        <button
+                          className="pdf-toolbar-btn"
+                          title="Zoom In"
+                          onClick={this.increaseScale}
+                        >
+                          <Icon name={zoomInSVG} size="15px" />
+                        </button>
+                        <div className="scale-separator" />
+                        <button
+                          className="pdf-toolbar-btn"
+                          title="Zoom Out"
+                          onClick={this.decreaseScale}
+                        >
+                          <Icon name={zoomOutSVG} size="15px" />
+                        </button>
+                        <p className="scale-ratio">
+                          {this.state.scale_ratio + '%'}
+                        </p>
+                      </div>
+                      <div>
+                        <a href={dataUrl}>
+                          <button className="pdf-toolbar-btn" title="Download">
+                            <Icon name={downloadSVG} size="15px" />
+                          </button>
+                        </a>
+                      </div>
+                    </div>
+                    <p>{this.state.page}</p>
 
-                   <LoadablePDFViewer
-                     document={{
-                       url: dataUrl,
-                     }}
-                     scale={this.state.scale}
-                     css='pdf-viewer'
-                     navigation={CustomNavigation}
-                     />
-                 </div>
-               </div>
-             );
-           })()}
-         </>
+                    <LoadablePDFViewer
+                      document={{
+                        url: dataUrl,
+                      }}
+                      scale={this.state.scale}
+                      css="pdf-viewer"
+                      navigation={CustomNavigation}
+                      page={this.state.currentPage}
+                      onDocumentComplete={this.onDocumentComplete}
+                    />
+                  </div>
+                </div>
+              );
+            })()}
+          </>
         )}
       </div>
-    )
+    );
   }
 }
 
