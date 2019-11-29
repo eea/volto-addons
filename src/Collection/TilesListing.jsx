85438b998100b206dbce502336cdda5fb28168ec
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-29 11:06:06 +0200

    Bring breadcrumb in tiles listing

--- src/Collection/TilesListing.jsx
+++ src/Collection/TilesListing.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FormattedDate } from 'react-intl';
 import { Item } from 'semantic-ui-react';
+import { Breadcrumb } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const TilesListing = ({ items }) => {
   const categoryStyle = {
@@ -21,6 +23,28 @@ const TilesListing = ({ items }) => {
                     <Item.Header as="a" href={item.url}>
                       {item.Title || item.title}
                     </Item.Header>
+                    {item['@components'] && item['@components'].breadcrumbs && (
+                      <div className="card-content">
+                        <Breadcrumb>
+                          {item['@components'].breadcrumbs.items.map(
+                            (item, index, items) => [
+                              index < items.length - 1 ? (
+                                <Breadcrumb.Section key={item['@id']}>
+                                  <Link to={item['@id']}>{item.title}</Link>
+                                  <Breadcrumb.Divider
+                                    key={`divider-${item.url}`}
+                                  />
+                                </Breadcrumb.Section>
+                              ) : (
+                                <Breadcrumb.Section key={item['@id']}>
+                                  <Link to={item['@id']}>{item.title}</Link>
+                                </Breadcrumb.Section>
+                              ),
+                            ],
+                          )}
+                        </Breadcrumb>
+                      </div>
+                    )}
                     <Item.Description>{item.description}</Item.Description>
                     <Item.Extra>
                       <span>
