3d74f3a2198a567cfdf43e45281170ced60f7258
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2019-11-29 14:22:39 +0200

    Collection listing styling

--- src/Collection/TilesListing.jsx
+++ src/Collection/TilesListing.jsx
@@ -1,72 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FormattedDate } from 'react-intl';
 import { Item } from 'semantic-ui-react';
 import { Breadcrumb } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { settings } from '~/config';
 
-const TilesListing = ({ items }) => {
-  const categoryStyle = {
-    color: '#252525',
-  };
-  const segmentStyle = {
-    marginBottom: '1rem',
-  };
+import './style.css';
 
-  return (
-    <div>
-      <div style={segmentStyle}>
-        <Item.Group>
-          {items.length
-            ? items.map(item => (
-                <Item key={item['@id']}>
-                  <Item.Content>
-                    <Item.Header as="a" href={item.url}>
-                      {item.Title || item.title}
-                    </Item.Header>
-                    {item['@components'] && item['@components'].breadcrumbs && (
-                      <div className="card-content">
+class TilesListing extends Component {
+  getPath(url) {
+    return url
+      .replace(settings.apiPath, '')
+      .replace(settings.internalApiPath, '');
+  }
+
+  render() {
+    const { items } = this.props;
+
+    return (
+      <div>
+        <div className="tile-listing">
+          <Item.Group>
+            {items.length
+              ? items.map(item => (
+                  <Item key={item['@id']}>
+                    <Item.Content>
+                      <Item.Header as="a" href={item.url}>
+                        <h3 className="tile-title">{item.Title || item.title}</h3>
+                      </Item.Header>
+                      {item['@components'] && item['@components'].breadcrumbs && (
                         <Breadcrumb>
                           {item['@components'].breadcrumbs.items
                             .slice(0, -1)
                             .map((item, index, items) => [
                               index < items.length - 1 ? (
-                                <Breadcrumb.Section key={item['@id']}>
-                                  <Link to={item['@id']}>{item.title}</Link>
+                                <Breadcrumb.Section>
+                                  <Link key={item.url} to={this.getPath(item['@id'])}>
+                                    {item.title}
+                                  </Link>
                                   <Breadcrumb.Divider
                                     key={`divider-${item.url}`}
                                   />
                                 </Breadcrumb.Section>
                               ) : (
-                                <Breadcrumb.Section key={item['@id']}>
-                                  <Link to={item['@id']}>{item.title}</Link>
+                                <Breadcrumb.Section>
+                                  <Link key={item.url} to={this.getPath(item['@id'])}>
+                                    {item.title}
+                                  </Link>
                                 </Breadcrumb.Section>
                               ),
                             ])}
                         </Breadcrumb>
-                      </div>
-                    )}
-                    <Item.Description>{item.description}</Item.Description>
-                    <Item.Extra>
-                      <span>
-                        Updated:{' '}
-                        <span style={categoryStyle}>
-                          <FormattedDate
-                            value={item.created}
-                            day="2-digit"
-                            month="long"
-                            year="numeric"
-                          />
-                        </span>
-                      </span>
-                    </Item.Extra>
-                  </Item.Content>
-                </Item>
-              ))
-            : 'No results'}
-        </Item.Group>
+                      )}
+                      <Item.Description>{item.description}</Item.Description>
+                      <Item.Extra>
+                        <span className="muted">Updated:</span>
+                        <FormattedDate
+                          value={item.ModificationDate}
+                          day="2-digit"
+                          month="long"
+                          year="numeric"
+                        />
+                      </Item.Extra>
+                    </Item.Content>
+                  </Item>
+                ))
+              : 'No results'}
+          </Item.Group>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default TilesListing;
