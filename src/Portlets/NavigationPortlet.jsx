049625e74769049202d22c1e7d074547dc30e1a7
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-16 18:42:49 +0200

    Added some styling for nav portlet

--- src/Portlets/NavigationPortlet.jsx
+++ src/Portlets/NavigationPortlet.jsx
@@ -3,6 +3,8 @@ import { List } from 'semantic-ui-react';
 import { Link as RouterLink } from 'react-router-dom';
 import { settings } from '~/config';
 
+import './styles.css';
+
 function Link({ href, ...props }) {
   const url =
     (href &&
@@ -10,7 +12,31 @@ function Link({ href, ...props }) {
         .replace(settings.apiPath, '')
         .replace(settings.internalApiPath, '')) ||
     '';
-  return <RouterLink to={url}>{props.children}</RouterLink>;
+  return (
+    <RouterLink to={url} title={props.title}>
+      {props.children}
+    </RouterLink>
+  );
+}
+
+function renderNode(node) {
+  // console.log('render node', node);
+  return (
+    <List.Item key={node['@id']} active={node.is_current}>
+      {node.is_current ? <List.Content floated="right">&gt;</List.Content> : ''}
+      <List.Content>
+        <List.Header>
+          <Link href={node.href} title={node.description}>
+            {node.title}
+          </Link>
+        </List.Header>
+        {(node.items.length && (
+          <List.List>{node.items.map(renderNode)}</List.List>
+        )) ||
+          ''}
+      </List.Content>
+    </List.Item>
+  );
 }
 
 export default ({ portlet }) => {
@@ -18,17 +44,5 @@ export default ({ portlet }) => {
     (portlet && portlet.navigationportlet && portlet.navigationportlet.items) ||
     [];
   console.log('nav items', items);
-  return (
-    <List className="navigation-portlet">
-      {items.map(item => (
-        <List.Item key={item['@id']}>
-          <List.Content>
-            <List.Header>
-              <Link href={item.href}>{item.title}</Link>
-            </List.Header>
-          </List.Content>
-        </List.Item>
-      ))}
-    </List>
-  );
+  return <List className="navigation-portlet">{items.map(renderNode)}</List>;
 };
