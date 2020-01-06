bc603fb000bf0002558bd0b740533dcb6b93e7a4
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-06 10:58:12 +0200

    Some improvements to collection_years widget

--- src/Widgets/CollectionYears.jsx
+++ src/Widgets/CollectionYears.jsx
@@ -5,39 +5,50 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Grid, Input, Label, Icon as IconOld, Dropdown } from 'semantic-ui-react';
+import {
+  Form,
+  Grid,
+  Label,
+  Icon as IconOld,
+  Dropdown,
+} from 'semantic-ui-react';
 import { map } from 'lodash';
 import { defineMessages, injectIntl } from 'react-intl';
 import { Icon } from '@plone/volto/components';
-import { years } from './utils/constants';
+import { range } from 'lodash';
 
 const messages = defineMessages({
-    default: {
-        id: 'Default',
-        defaultMessage: 'Default',
-    },
-    idTitle: {
-        id: 'Short Name',
-        defaultMessage: 'Short Name',
-    },
-    idDescription: {
-        id: 'Used for programmatic access to the fieldset.',
-        defaultMessage: 'Used for programmatic access to the fieldset.',
-    },
-    title: {
-        id: 'Title',
-        defaultMessage: 'Title',
-    },
-    description: {
-        id: 'Description',
-        defaultMessage: 'Description',
-    },
-    required: {
-        id: 'Required',
-        defaultMessage: 'Required',
-    },
+  default: {
+    id: 'Default',
+    defaultMessage: 'Default',
+  },
+  idTitle: {
+    id: 'Short Name',
+    defaultMessage: 'Short Name',
+  },
+  idDescription: {
+    id: 'Used for programmatic access to the fieldset.',
+    defaultMessage: 'Used for programmatic access to the fieldset.',
+  },
+  title: {
+    id: 'Title',
+    defaultMessage: 'Title',
+  },
+  description: {
+    id: 'Description',
+    defaultMessage: 'Description',
+  },
+  required: {
+    id: 'Required',
+    defaultMessage: 'Required',
+  },
 });
 
+const currentYear = new Date().getFullYear();
+const years = range(1929, currentYear).map(y => ({
+  text: y,
+  value: y,
+}));
 
 /**
  * CollectionYears component class.
@@ -45,195 +56,204 @@ const messages = defineMessages({
  * @extends Component
  */
 class CollectionYears extends Component {
-    /**
-     * Property types.
-     * @property {Object} propTypes Property types.
-     * @static
-     */
-    static propTypes = {
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string,
-        required: PropTypes.bool,
-        error: PropTypes.arrayOf(PropTypes.string),
-        value: PropTypes.string,
-        focus: PropTypes.bool,
-        onChange: PropTypes.func,
-        onEdit: PropTypes.func,
-        onDelete: PropTypes.func,
-        icon: PropTypes.shape({
-            xmlns: PropTypes.string,
-            viewBox: PropTypes.string,
-            content: PropTypes.string,
-        }),
-        iconAction: PropTypes.func,
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    required: PropTypes.bool,
+    error: PropTypes.arrayOf(PropTypes.string),
+    value: PropTypes.string,
+    focus: PropTypes.bool,
+    onChange: PropTypes.func,
+    onEdit: PropTypes.func,
+    onDelete: PropTypes.func,
+    icon: PropTypes.shape({
+      xmlns: PropTypes.string,
+      viewBox: PropTypes.string,
+      content: PropTypes.string,
+    }),
+    iconAction: PropTypes.func,
+  };
+
+  /**
+   * Default properties.
+   * @property {Object} defaultProps Default properties.
+   * @static
+   */
+  static defaultProps = {
+    description: null,
+    required: false,
+    error: [],
+    value: null,
+    onChange: null,
+    onEdit: null,
+    onDelete: null,
+    focus: false,
+    icon: null,
+    iconAction: null,
+  };
+
+  /**
+   * Component did mount lifecycle method
+   * @method componentDidMount
+   * @returns {undefined}
+   */
+  componentDidMount() {
+    if (this.props.focus) {
+      this.node.focus();
+    }
+  }
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    const {
+      id,
+      title,
+      required,
+      description,
+      error,
+      value,
+      // onChange,
+      onEdit,
+      onDelete,
+      intl,
+      icon,
+      iconAction,
+      fieldSet,
+    } = this.props;
+
+    const schema = {
+      fieldsets: [
+        {
+          id: 'default',
+          title: intl.formatMessage(messages.default),
+          fields: ['title', 'id', 'description', 'required'],
+        },
+      ],
+      properties: {
+        id: {
+          type: 'string',
+          title: intl.formatMessage(messages.idTitle),
+          description: intl.formatMessage(messages.idDescription),
+        },
+        title: {
+          type: 'string',
+          title: intl.formatMessage(messages.title),
+        },
+        description: {
+          type: 'string',
+          widget: 'textarea',
+          title: intl.formatMessage(messages.description),
+        },
+        required: {
+          type: 'boolean',
+          title: intl.formatMessage(messages.required),
+        },
+      },
+      required: ['id', 'title'],
     };
 
-    /**
-     * Default properties.
-     * @property {Object} defaultProps Default properties.
-     * @static
-     */
-    static defaultProps = {
-        description: null,
-        required: false,
-        error: [],
-        value: null,
-        onChange: null,
-        onEdit: null,
-        onDelete: null,
-        focus: false,
-        icon: null,
-        iconAction: null,
-    };
+    return (
+      <Form.Field
+        inline
+        required={required}
+        error={error.length > 0}
+        className={description ? 'help text' : 'text'}
+        id={`${fieldSet || 'field'}-${id}`}
+      >
+        <Grid>
+          <Grid.Row stretched>
+            <Grid.Column width="4">
+              <div className="wrapper">
+                <label htmlFor={`field-${id}`}>
+                  {onEdit && (
+                    <i
+                      aria-hidden="true"
+                      className="grey bars icon drag handle"
+                    />
+                  )}
+                  {title}
+                </label>
+              </div>
+            </Grid.Column>
+            <Grid.Column width="8">
+              {onEdit && (
+                <div className="toolbar">
+                  <button
+                    className="item ui noborder button"
+                    onClick={() => onEdit(id, schema)}
+                  >
+                    <IconOld name="write square" size="large" color="blue" />
+                  </button>
+                  <button
+                    aria-label="Delete"
+                    className="item ui noborder button"
+                    onClick={() => onDelete(id)}
+                  >
+                    <IconOld name="close" size="large" color="red" />
+                  </button>
+                </div>
+              )}
 
+              <Dropdown
+                placeholder="Start Year"
+                search
+                selection
+                options={years}
+                value={value && value.start_year}
+                onChange={(e, data) =>
+                  this.props.onChange(id, {
+                    end_year: value && value.end_year,
+                    start_year: data.value,
+                  })
+                }
+              />
 
-    /**
-     * Component did mount lifecycle method
-     * @method componentDidMount
-     * @returns {undefined}
-     */
-    componentDidMount() {
-        if (this.props.focus) {
-            this.node.focus();
-        }
-    }
-
-    /**
- * Render method.
- * @method render
- * @returns {string} Markup for the component.
- */
-    render() {
-        const {
-            id,
-            title,
-            required,
-            description,
-            error,
-            value,
-            onChange,
-            onEdit,
-            onDelete,
-            intl,
-            icon,
-            iconAction,
-            fieldSet,
-        } = this.props;
-
-        const schema = {
-            fieldsets: [
-                {
-                    id: 'default',
-                    title: intl.formatMessage(messages.default),
-                    fields: ['title', 'id', 'description', 'required'],
-                },
-            ],
-            properties: {
-                id: {
-                    type: 'string',
-                    title: intl.formatMessage(messages.idTitle),
-                    description: intl.formatMessage(messages.idDescription),
-                },
-                title: {
-                    type: 'string',
-                    title: intl.formatMessage(messages.title),
-                },
-                description: {
-                    type: 'string',
-                    widget: 'textarea',
-                    title: intl.formatMessage(messages.description),
-                },
-                required: {
-                    type: 'boolean',
-                    title: intl.formatMessage(messages.required),
-                },
-            },
-            required: ['id', 'title'],
-        };
-
-        return (
-            <Form.Field
-                inline
-                required={required}
-                error={error.length > 0}
-                className={description ? 'help text' : 'text'}
-                id={`${fieldSet || 'field'}-${id}`}
-            >
-                <Grid>
-                    <Grid.Row stretched>
-                        <Grid.Column width="4">
-                            <div className="wrapper">
-                                <label htmlFor={`field-${id}`}>
-                                    {onEdit && (
-                                        <i
-                                            aria-hidden="true"
-                                            className="grey bars icon drag handle"
-                                        />
-                                    )}
-                                    {title}
-                                </label>
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column width="8">
-                            {onEdit && (
-                                <div className="toolbar">
-                                    <button
-                                        className="item ui noborder button"
-                                        onClick={() => onEdit(id, schema)}
-                                    >
-                                        <IconOld name="write square" size="large" color="blue" />
-                                    </button>
-                                    <button
-                                        aria-label="Delete"
-                                        className="item ui noborder button"
-                                        onClick={() => onDelete(id)}
-                                    >
-                                        <IconOld name="close" size="large" color="red" />
-                                    </button>
-                                </div>
-                            )}
-
-                            <Dropdown
-                                placeholder='Start Year'
-                                search
-                                selection
-                                options={years}
-                                value={value.start_year}
-                                onChange={(e, data) => this.props.onChange(id, { "end_year": value.end_year, "start_year": data.value })}
-                            />
-
-                            <Dropdown
-                                placeholder='End Year'
-                                search
-                                selection
-                                options={years}
-                                value={value.end_year}
-                                onChange={(e, data) => this.props.onChange(id, { "end_year": data.value, "start_year": value.start_year })}
-                            />
-                            {map(error, message => (
-                                <Label key={message} basic color="red" pointing>
-                                    {message}
-                                </Label>
-                            ))}
-                            {icon && iconAction && (
-                                <button onClick={iconAction}>
-                                    <Icon name={icon} size="18px" />
-                                </button>
-                            )}
-                        </Grid.Column>
-                    </Grid.Row>
-                    {description && (
-                        <Grid.Row stretched>
-                            <Grid.Column stretched width="12">
-                                <p className="help">{description}</p>
-                            </Grid.Column>
-                        </Grid.Row>
-                    )}
-                </Grid>
-            </Form.Field>
-        );
-    }
+              <Dropdown
+                placeholder="End Year"
+                search
+                selection
+                options={years}
+                value={value && value.end_year}
+                onChange={(e, data) =>
+                  this.props.onChange(id, {
+                    end_year: data.value,
+                    start_year: value && value.start_year,
+                  })
+                }
+              />
+              {map(error, message => (
+                <Label key={message} basic color="red" pointing>
+                  {message}
+                </Label>
+              ))}
+              {icon && iconAction && (
+                <button onClick={iconAction}>
+                  <Icon name={icon} size="18px" />
+                </button>
+              )}
+            </Grid.Column>
+          </Grid.Row>
+          {description && (
+            <Grid.Row stretched>
+              <Grid.Column stretched width="12">
+                <p className="help">{description}</p>
+              </Grid.Column>
+            </Grid.Row>
+          )}
+        </Grid>
+      </Form.Field>
+    );
+  }
 }
 
 export default injectIntl(CollectionYears);
