7bb8c4a73e0f5c16c6ed710ab44f2d070976000a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-24 11:55:06 +0200

    Added Spinner

---
+++ src/Spinner/Spinner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Spinner = () => (
+  <div id="floatBarsG" style={{ margin: '1rem auto' }}>
+    <div id="floatBarsG_1" className="floatBarsG" />
+    <div id="floatBarsG_2" className="floatBarsG" />
+    <div id="floatBarsG_3" className="floatBarsG" />
+    <div id="floatBarsG_4" className="floatBarsG" />
+    <div id="floatBarsG_5" className="floatBarsG" />
+    <div id="floatBarsG_6" className="floatBarsG" />
+    <div id="floatBarsG_7" className="floatBarsG" />
+    <div id="floatBarsG_8" className="floatBarsG" />
+  </div>
+);
+
+export default Spinner;
