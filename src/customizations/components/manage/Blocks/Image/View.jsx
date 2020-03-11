db4a2c064f961a7bb1c5df895f2af57a747b6009
Mihai Macaneata <themacaneata@gmail.com>
2020-03-11 16:42:54 +0200

    image modal onclick

---
+++ src/customizations/components/manage/Blocks/Image/View.jsx
@@ -0,0 +1,148 @@
+/**
+ * View image block.
+ * @module components/manage/Blocks/Image/View
+ */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import cx from 'classnames';
+import { settings } from '~/config';
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
+
+import { Icon } from '@plone/volto/components';
+import ImageFull from '@plone/volto/icons/image-full.svg';
+
+import { flattenToAppURL } from '@plone/volto/helpers';
+
+/**
+ * View image block class.
+ * @class View
+ * @extends Component
+ */
+const View = ({ data, detached }) => {
+
+  const [zoomed, setZoomed] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  return (<p
+    className={cx(
+      'block image align',
+      {
+        center: !Boolean(data.align),
+        detached,
+      },
+      data.align,
+    )}
+  >
+    {data.url && (
+      <>
+        {(() => {
+          const image = (
+            <React.Fragment>
+              <img
+                  className={cx({ 'full-width': data.align === 'full' })}
+                  style={{
+                    width: data.width ? data.width + 'px' : 'auto',
+                    height: data.height ? data.height + 'px' : 'auto',
+                    marginLeft:
+                      data.inLeftColumn && data.width
+                        ? `-${parseInt(data.width) + 10}px`
+                        : '0',
+                    marginRight: data.inLeftColumn ? '0!important' : '1rem',
+                  }}
+                  src={data.url.includes(settings.apiPath)
+                    ? `${flattenToAppURL(data.url)}/@@images/image`
+                    : data.url}
+                  onClick={() => setZoomed(true)}
+                  alt={data.alt || ''}
+                  onMouseEnter={() => setHovered(true)}
+                  onMouseLeave={() => setHovered(false)}
+                ></img>
+              <Modal
+                style={{ width: 'unset' }}
+                onClose={() => setZoomed(false)} trigger={(
+                <div
+                style={{
+                  position: 'absolute',
+                  // marginTop: '-2.2rem',
+                  color: 'white',
+                  cursor: 'pointer',
+                  margin: '.5rem',
+                  transition: '100ms opacity',
+                  opacity: hovered ? '1' : '0',
+                }}
+                onClick={(e) => e.preventDefault}
+                onMouseEnter={() => setHovered(true)}
+                // onMouseLeave={() => setHovered(false
+                >
+
+                <Icon
+                    onClick={() => setZoomed(true)}
+                    name={ImageFull} 
+                    
+                    size="24px" />
+                    </div>
+                    )}>
+                  
+                <Modal.Content image>
+                  <img
+                    className={cx({ 'full-width': data.align === 'full' })}
+                    zoomed={zoomed}
+                    style={{ maxHeight: '80vh', maxWidth: '100%' }}
+                    src={data.url.includes(settings.apiPath)
+                      ? `${flattenToAppURL(data.url)}/@@images/image`
+                      : data.url}
+                    onClick={() => setZoomed(true)}
+                    onRequestClose={() => setZoomed(false)}
+                    alt={data.alt || ''}
+                  onMouseLeave={() => setHovered(false)}
+
+                  ></img>
+                </Modal.Content>
+              </Modal>
+            </React.Fragment>
+          );
+          if (data.href) {
+            if (
+              (data.href.startsWith('http') || data.href.startsWith('https')) &&
+              !data.href.includes(settings.apiPath)
+            ) {
+              return (
+                <a
+                  target={data.openLinkInNewTab ? '_blank' : null}
+                  href={data.href}
+                >
+                  {image}
+                </a>
+              );
+            } else {
+              return (
+                <Link
+                  to={data.href.replace(settings.apiPath, '')}
+                  target={data.openLinkInNewTab ? '_blank' : null}
+                >
+                  {image}
+                </Link>
+              );
+            }
+          } else {
+            return image;
+          }
+        })()}
+      </>
+    )}
+  </p>
+  )
+}
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+View.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default View;
