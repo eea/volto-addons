ea156db6e7e26355a01b581a5656be12dd27a66f
Mihai Macaneata <themacaneata@gmail.com>
2020-03-11 17:06:12 +0200

    link support

--- src/customizations/components/manage/Blocks/Image/View.jsx
+++ src/customizations/components/manage/Blocks/Image/View.jsx
@@ -24,7 +24,32 @@ const View = ({ data, detached }) => {
 
   const [zoomed, setZoomed] = useState(false);
   const [hovered, setHovered] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const trigger = (
+    <div
+      style={{
+        position: 'absolute',
+        // marginTop: '-2.2rem',
+        color: 'white',
+        cursor: 'pointer',
+        margin: '.5rem',
+        transition: '100ms opacity',
+        opacity: hovered ? '1' : '0',
+      }}
+      onClick={(e) => e.preventDefault}
+      onMouseEnter={() => setHovered(true)}
+    // onMouseLeave={() => setHovered(false
+    >
 
+      <Icon
+        onClick={() => {
+          setModalOpen(true); setZoomed(true)
+        }}
+        name={ImageFull}
+
+        size="24px" />
+    </div>
+  )
   return (<p
     className={cx(
       'block image align',
@@ -41,50 +66,29 @@ const View = ({ data, detached }) => {
           const image = (
             <React.Fragment>
               <img
-                  className={cx({ 'full-width': data.align === 'full' })}
-                  style={{
-                    width: data.width ? data.width + 'px' : 'auto',
-                    height: data.height ? data.height + 'px' : 'auto',
-                    marginLeft:
-                      data.inLeftColumn && data.width
-                        ? `-${parseInt(data.width) + 10}px`
-                        : '0',
-                    marginRight: data.inLeftColumn ? '0!important' : '1rem',
-                  }}
-                  src={data.url.includes(settings.apiPath)
-                    ? `${flattenToAppURL(data.url)}/@@images/image`
-                    : data.url}
-                  onClick={() => setZoomed(true)}
-                  alt={data.alt || ''}
-                  onMouseEnter={() => setHovered(true)}
-                  onMouseLeave={() => setHovered(false)}
-                ></img>
+                className={cx({ 'full-width': data.align === 'full' })}
+                style={{
+                  width: data.width ? data.width + 'px' : 'auto',
+                  height: data.height ? data.height + 'px' : 'auto',
+                  marginLeft:
+                    data.inLeftColumn && data.width
+                      ? `-${parseInt(data.width) + 10}px`
+                      : '0',
+                  marginRight: data.inLeftColumn ? '0!important' : '1rem',
+                }}
+                src={data.url.includes(settings.apiPath)
+                  ? `${flattenToAppURL(data.url)}/@@images/image`
+                  : data.url}
+                onClick={() => setZoomed(true)}
+                alt={data.alt || ''}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+              ></img>
               <Modal
                 style={{ width: 'unset' }}
-                onClose={() => setZoomed(false)} trigger={(
-                <div
-                style={{
-                  position: 'absolute',
-                  // marginTop: '-2.2rem',
-                  color: 'white',
-                  cursor: 'pointer',
-                  margin: '.5rem',
-                  transition: '100ms opacity',
-                  opacity: hovered ? '1' : '0',
-                }}
-                onClick={(e) => e.preventDefault}
-                onMouseEnter={() => setHovered(true)}
-                // onMouseLeave={() => setHovered(false
-                >
-
-                <Icon
-                    onClick={() => setZoomed(true)}
-                    name={ImageFull} 
-                    
-                    size="24px" />
-                    </div>
-                    )}>
-                  
+                open={modalOpen}
+                onClose={() => { setZoomed(false); setModalOpen(false) }}
+              >
                 <Modal.Content image>
                   <img
                     className={cx({ 'full-width': data.align === 'full' })}
@@ -96,7 +100,7 @@ const View = ({ data, detached }) => {
                     onClick={() => setZoomed(true)}
                     onRequestClose={() => setZoomed(false)}
                     alt={data.alt || ''}
-                  onMouseLeave={() => setHovered(false)}
+                    onMouseLeave={() => setHovered(false)}
 
                   ></img>
                 </Modal.Content>
@@ -109,25 +113,38 @@ const View = ({ data, detached }) => {
               !data.href.includes(settings.apiPath)
             ) {
               return (
-                <a
-                  target={data.openLinkInNewTab ? '_blank' : null}
-                  href={data.href}
-                >
-                  {image}
-                </a>
+                <React.Fragment>
+
+                  <a
+                    target={data.openLinkInNewTab ? '_blank' : null}
+                    href={data.href}
+                  >
+                    {image}
+                  </a>
+                  {trigger}
+                </React.Fragment>
+
               );
             } else {
               return (
-                <Link
-                  to={data.href.replace(settings.apiPath, '')}
-                  target={data.openLinkInNewTab ? '_blank' : null}
-                >
-                  {image}
-                </Link>
+                <React.Fragment>
+                  <Link
+                    to={data.href.replace(settings.apiPath, '')}
+                    target={data.openLinkInNewTab ? '_blank' : null}
+                  >
+                    {image}
+                  </Link>
+                  {trigger}
+                </React.Fragment>
               );
             }
           } else {
-            return image;
+            return (
+              <React.Fragment>
+                {image}
+                {trigger}
+
+              </React.Fragment>)
           }
         })()}
       </>
