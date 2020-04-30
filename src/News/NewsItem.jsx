f50016a7a17107cdd578a1060e99f9a31bc9bff5
Miu Razvan <miu.razvan28@gmail.com>
2020-04-30 13:35:53 +0300

    Events start/end, location added

--- src/News/NewsItem.jsx
+++ src/News/NewsItem.jsx
@@ -9,9 +9,23 @@ const NewsItem = ({ item }) => {
     return `${mo} ${da} ${ye}`
   }
 
+  const prettyDateTime = (time) => {
+    return Intl.DateTimeFormat('en', {
+      weekday: 'short',
+      day: '2-digit',
+      month: 'short',
+      hour: '2-digit',
+      minute: '2-digit'
+    }).format(new Date(time));
+  }
+
   const itemPath = (urlString) => {
     const url = new URL(urlString);
     return url.pathname.replace('/fise', '');
+  }
+  console.log(item)
+  if (item.start && item.end) {
+    console.log(new Date(item.start))
   }
 
   return (
@@ -21,10 +35,28 @@ const NewsItem = ({ item }) => {
       </Link>
       <div className="expanded article-body">
         <div className={'meta-data'}>
-          {item.effective && (
+          {item.effective && !(item.start && item.end) && (
             <div className="text-tab">
               <span className="format-text">Published: </span>
               <span className="format-type">{prettyDate(item.effective)}</span>
+            </div>
+          )}
+          {item.start && item.end && (
+            <div className="event-dates">
+              <div className="text-tab">
+                <span className="format-text">Starting: </span>
+                <span className="format-type">{prettyDateTime(item.start)}</span>
+              </div>
+              <div className="text-tab">
+                <span className="format-text">Ending: </span>
+                <span className="format-type">{prettyDateTime(item.end)}</span>
+              </div>
+            </div>
+          )}
+          {item.location && (
+            <div className="text-tab">
+              <span className="format-text">Location: </span>
+              <span className="format-type">{item.location}</span>
             </div>
           )}
         </div>
