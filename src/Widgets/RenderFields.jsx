315714b1289a9fa9dd6adbf1cc0e9ec9830b097f
Miu Razvan <miu.razvan28@gmail.com>
2020-07-16 18:03:35 +0300

    Added sql type in RenderFields

--- src/Widgets/RenderFields.jsx
+++ src/Widgets/RenderFields.jsx
@@ -124,56 +124,6 @@ const RenderFields = props => {
             ) : (
               ''
             )}
-            {/* DATA QUERY TYPE */}
-            {schema[key].type === 'data-query' ? (
-              <Segment className="form sidebar-image-data">
-                <div>
-                  <p style={{ padding: '1rem', margin: '0', fontSize: '16px' }}>
-                    Additional query
-                  </p>
-                  <TextWidget
-                    id={`data-query-widget-column-${key}_i`}
-                    title={schema[key].iTitle}
-                    required={false}
-                    onChange={(id, value) =>
-                      props.onChangeBlock(props.block, {
-                        ...props.data,
-                        [key]: {
-                          ...props.data?.[key],
-                          value: {
-                            ...props.data?.[key]?.value,
-                            i: value || '',
-                            o: 'plone.app.querystring.operation.selection.any',
-                          },
-                        },
-                      })
-                    }
-                    value={props.data?.[key]?.value?.i}
-                  />
-                  <ArrayWidget
-                    id={`data-query-widget-column-${key}_v`}
-                    title={schema[key].vTitle}
-                    required={false}
-                    onChange={(id, value) =>
-                      props.onChangeBlock(props.block, {
-                        ...props.data,
-                        [key]: {
-                          ...props.data?.[key],
-                          value: {
-                            ...props.data?.[key]?.value,
-                            v: value || [],
-                            o: 'plone.app.querystring.operation.selection.any',
-                          },
-                        },
-                      })
-                    }
-                    value={props.data?.[key]?.value?.v}
-                  />
-                </div>
-              </Segment>
-            ) : (
-              ''
-            )}
             {/* SQL TYPE */}
             {schema[key].type === 'sql' ? (
               <Segment className="form sidebar-image-data">
@@ -185,7 +135,7 @@ const RenderFields = props => {
                       fontSize: '16px',
                     }}
                   >
-                    SQL Select
+                    {schema[key].title}
                   </p>
                   <p
                     style={{
@@ -195,64 +145,69 @@ const RenderFields = props => {
                     }}
                     className="info"
                   >
-                    SELECT * FROM "{props.data?.[key]?.value?.table}" WHERE "
-                    {props.data?.[key]?.value?.columnKey}" = "
-                    {props.data?.[key]?.value?.columnValue}"
+                    SELECT * FROM "{props.data?.[key]?.selectQuery?.table}"
+                    WHERE "{props.data?.[key]?.selectQuery?.columnKey}" = "
+                    {props.data?.[key]?.selectQuery?.columnValue}"
+                    {props.data?.[key]?.additionalQuery && (
+                      <span>
+                        {' '}
+                        AND "{props.data?.[key]?.additionalQuery?.columnKey}" =
+                        "{props.data?.[key]?.additionalQuery?.columnValue}"
+                      </span>
+                    )}
                   </p>
-                  <TextWidget
-                    id={`sql-widget-column-${key}-1`}
-                    title="Table"
-                    required={false}
-                    onChange={(id, value) => {
-                      props.onChangeBlock(props.block, {
-                        ...props.data,
-                        [key]: {
-                          ...props.data?.[key],
-                          value: {
-                            ...props.data?.[key]?.value,
-                            table: value,
+                  {schema[key].selectQueryFields?.map((field, index) => (
+                    <TextWidget
+                      id={`additional-query-widget-column-${index}`}
+                      key={`additional-query-widget-column-${index}`}
+                      title={field.title}
+                      required={false}
+                      onChange={(id, value) => {
+                        props.onChangeBlock(props.block, {
+                          ...props.data,
+                          [key]: {
+                            ...props.data?.[key],
+                            selectQuery: {
+                              ...props.data?.[key]?.selectQuery,
+                              [field.id]: value,
+                            },
                           },
-                        },
-                      });
-                    }}
-                    value={props.data?.[key]?.value?.table}
-                  />
-                  <TextWidget
-                    id={`sql-widget-column-${key}-2`}
-                    title="Where column"
-                    required={false}
-                    onChange={(id, value) => {
-                      props.onChangeBlock(props.block, {
-                        ...props.data,
-                        [key]: {
-                          ...props.data?.[key],
-                          value: {
-                            ...props.data?.[key]?.value,
-                            columnKey: value,
+                        });
+                      }}
+                      value={props.data?.[key]?.selectQuery?.[field.id]}
+                    />
+                  ))}
+                  {schema[key].additionalQueryFields && (
+                    <p
+                      style={{
+                        padding: '1rem 1rem 0 1rem',
+                        fontSize: '16px',
+                      }}
+                    >
+                      Additional query
+                    </p>
+                  )}
+                  {schema[key].additionalQueryFields?.map((field, index) => (
+                    <TextWidget
+                      id={`select-query-widget-column-${index}`}
+                      key={`select-query-widget-column-${index}`}
+                      title={field.title}
+                      required={false}
+                      onChange={(id, value) => {
+                        props.onChangeBlock(props.block, {
+                          ...props.data,
+                          [key]: {
+                            ...props.data?.[key],
+                            additionalQuery: {
+                              ...props.data?.[key]?.additionalQuery,
+                              [field.id]: value,
+                            },
                           },
-                        },
-                      });
-                    }}
-                    value={props.data?.[key]?.value?.columnKey}
-                  />
-                  <TextWidget
-                    id={`sql-widget-column-${key}-3`}
-                    title="Is equal to"
-                    required={false}
-                    onChange={(id, value) => {
-                      props.onChangeBlock(props.block, {
-                        ...props.data,
-                        [key]: {
-                          ...props.data?.[key],
-                          value: {
-                            ...props.data?.[key]?.value,
-                            columnValue: value,
-                          },
-                        },
-                      });
-                    }}
-                    value={props.data?.[key]?.value?.columnValue}
-                  />
+                        });
+                      }}
+                      value={props.data?.[key]?.additionalQuery?.[field.id]}
+                    />
+                  ))}
                 </div>
               </Segment>
             ) : (
