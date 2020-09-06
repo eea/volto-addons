431263fb0000fe402a6d9bdae3f0e7c73ca5a00d
Miu Razvan <miu.razvan28@gmail.com>
2020-09-07 01:25:53 +0300

    Updated ExpandableList

--- src/ExpendableList/Edit.jsx
+++ src/ExpendableList/Edit.jsx
@@ -2,76 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import _uniqueId from 'lodash/uniqueId';
-import RenderFields from 'volto-addons/Widgets/RenderFields';
+import InlineForm from '@plone/volto/components/manage/Form/InlineForm';
+import { SidebarPortal } from '@plone/volto/components';
 import View from './View';
 import { settings } from '~/config';
+import { makeSchema } from './schema';
 
-const getSchema = props => {
-  const isExpandable = props.data.isExpandable?.value;
-  return {
-    ordered: {
-      title: 'Ordered',
-      type: 'boolean',
-    },
-    isExpandable: {
-      title: 'Is expandable',
-      type: 'boolean',
-    },
-    listClassname: {
-      title: 'List classname',
-      type: 'text',
-    },
-    listItemClassname: {
-      title: 'Item classname',
-      type: 'text',
-    },
-    items: {
-      title: 'List items',
-      type: 'schema',
-      fieldSetTitle: 'List items metadata',
-      fieldSetId: 'list_items_metadata',
-      fieldSetSchema: {
-        fieldsets: [
-          {
-            id: 'default',
-            title: 'title',
-            fields: ['title', 'id', 'description'],
-          },
-        ],
-        properties: {
-          title: {
-            title: 'Title',
-            type: 'text',
-          },
-          id: {
-            title: 'Id',
-            type: 'text',
-          },
-          description: {
-            title: 'Description',
-            widget: 'textarea',
-          },
-        },
-        required: () => {
-          if (isExpandable) return ['id', 'title', 'description'];
-          return ['id', 'title'];
-        },
-      },
-      editFieldset: false,
-      deleteFieldset: false,
-    },
-  };
-};
-
-const Edit = props => {
+const Edit = (props) => {
   const [state, setState] = useState({
-    schema: getSchema({ ...props, providerUrl: settings.providerUrl }),
+    schema: makeSchema({ ...props, providerUrl: settings.providerUrl }),
     id: _uniqueId('block_'),
   });
   useEffect(() => {
     setState({
       ...state,
-      schema: getSchema({
+      schema: makeSchema({
         ...props,
       }),
     });
@@ -79,8 +24,21 @@ const Edit = props => {
   }, [props.data.isExpandable])
   return (
     <div>
-      <RenderFields schema={state.schema} {...props} title="Navigation block" />
-      <View {...props} id={state.id} />
+      <SidebarPortal selected={props.selected}>
+        <InlineForm
+          schema={state.schema}
+          title={state.schema.title}
+          onChangeField={(field, data) => {
+            props.onChangeBlock(props.block, {
+              ...(props.data || {}),
+              [field]: data,
+            });
+          }}
+          formData={props.data || {}}
+          block={props.block}
+        />
+      </SidebarPortal>
+      <View {...props} id={state.id} mode="edit" />
     </div>
   );
 };
--- src/ExpendableList/View.jsx
+++ src/ExpendableList/View.jsx
@@ -2,135 +2,127 @@
 import React, { useState } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { List } from 'semantic-ui-react';
+import { Icon } from '@plone/volto/components';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import DOMPurify from 'dompurify';
+import move from 'lodash-move';
+import cx from 'classnames';
+import dragSVG from '@plone/volto/icons/drag.svg';
 import './style.css';
-
 const View = ({ content, ...props }) => {
   const [state, setState] = useState({
     activeItem: '',
   });
-  const isExpandable = props.data?.isExpandable?.value;
-  const ordered = props.data?.ordered?.value;
-  const listItemClassname = props.data?.listItemClassname?.value;
-  const listClassname = props.data?.listClassname?.value;
-  const items = props.data?.items?.value
-    ? JSON.parse(props.data.items.value).properties
-    : {};
+  const isExpandable = props.data?.isExpandable;
+  const listItemTitleClassname = props.data?.listItemTitleClassname;
+  const listItemDescriptionClassname = props.data?.listItemDescriptionClassname;
+  const listClassname = props.data?.listClassname;
+  const items = props.data?.items || [];
+  const ordered = props.data?.ordered;
 
-  if (ordered) {
-    return (
-      <ol
-        className={
-          listClassname ? `expendableList ${listClassname}` : 'expendableList'
-        }
-      >
-        {items
-          ? Object.entries(items).map(([key, value]) => {
-              return (
-                <li
-                  className={
-                    (listItemClassname ? listItemClassname : '') +
-                    (isExpandable ? 'expandable' : 'no-expandable')
-                  }
-                  onClick={() => {
-                    if (isExpandable && state.activeItem === key) {
-                      setState({ ...state, activeItem: '' });
-                    } else if (isExpandable) {
-                      setState({ ...state, activeItem: key });
-                    }
-                  }}
-                >
-                  <span
-                    className={
-                      'title ' +
-                      (isExpandable
-                        ? state.activeItem === key
-                          ? 'active'
-                          : ''
-                        : 'active')
-                    }
-                  >
-                    {value.title}
-                  </span>
-                  {/* eslint-disable-next-line */}
-                                <div
-                    className={
-                      'description ' +
-                      (isExpandable
-                        ? state.activeItem === key
-                          ? 'show'
-                          : 'hide'
-                        : '')
-                    }
-                    onClick={(e) => e.stopPropagation()}
-                    dangerouslySetInnerHTML={{
-                      __html: DOMPurify.sanitize(value.description),
-                    }}
-                  />
-                </li>
-              );
-            })
-          : ''}
-      </ol>
-    );
-  } else {
-    return (
-      <ul
-        className={
-          listClassname ? `expendableList ${listClassname}` : 'expendableList'
-        }
-      >
-        {items
-          ? Object.entries(items).map(([key, value]) => {
-              return (
-                <li
-                  className={
-                    (listItemClassname ? listItemClassname : '') +
-                    (isExpandable ? 'expandable' : 'no-expandable')
-                  }
-                  onClick={() => {
-                    if (isExpandable && state.activeItem === key) {
-                      setState({ ...state, activeItem: '' });
-                    } else if (isExpandable) {
-                      setState({ ...state, activeItem: key });
-                    }
-                  }}
-                >
-                  <span
-                    className={
-                      'title ' +
-                      (isExpandable
-                        ? state.activeItem === key
-                          ? 'active'
-                          : ''
-                        : 'active')
-                    }
-                  >
-                    {value.title}
-                  </span>
-                  {/* eslint-disable-next-line */}
-                                <div
-                    className={
-                      'description ' +
-                      (isExpandable
-                        ? state.activeItem === key
-                          ? 'show'
-                          : 'hide'
-                        : '')
-                    }
-                    onClick={(e) => e.stopPropagation()}
-                    dangerouslySetInnerHTML={{
-                      __html: DOMPurify.sanitize(value.description),
-                    }}
-                  />
-                </li>
-              );
-            })
-          : ''}
-      </ul>
-    );
-  }
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+    if (!destination) {
+      return;
+    }
+    if (source.droppableId === destination.droppableId) {
+      props.onChangeBlock(props.block, {
+        ...props.data,
+        items: move(props.data.items, source.index, destination.index),
+      });
+    } else {
+      //  WIP
+    }
+  };
+
+  return (
+    <div className={cx('expandable-list', listClassname)}>
+      {props.mode === 'edit' && !items.length ? <p>Expandable list</p> : ''}
+      {items ? (
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="list-item">
+            {(providedDroppable) => (
+              <>
+                <div ref={providedDroppable.innerRef}>
+                  {Object.entries(items).map(([key, value], index) => (
+                    <Draggable key={key} draggableId={key} index={index}>
+                      {(providedItem) => (
+                        <div
+                          ref={providedItem.innerRef}
+                          {...providedItem.draggableProps}
+                        >
+                          <div
+                            style={{
+                              position: 'relative',
+                              height: '100%',
+                            }}
+                          >
+                            <div
+                              style={{
+                                visibility: 'visible',
+                                display: 'inline-block',
+                              }}
+                              {...providedItem.dragHandleProps}
+                              className="drag handle wrapper"
+                            >
+                              <Icon name={dragSVG} size="18px" />
+                            </div>
+                            <div className="list-item" key={`list-item-${key}`}>
+                              <button
+                                className={cx(
+                                  'list-item-title',
+                                  listItemTitleClassname
+                                    ? listItemTitleClassname
+                                    : '',
+                                  isExpandable ? 'expandable' : 'no-expandable',
+                                )}
+                                onClick={() => {
+                                  if (
+                                    isExpandable &&
+                                    state.activeItem === key
+                                  ) {
+                                    setState({ ...state, activeItem: '' });
+                                  } else if (isExpandable) {
+                                    setState({ ...state, activeItem: key });
+                                  }
+                                }}
+                              >
+                                {ordered ? `${index + 1}. ` : ''}
+                                {value.title}
+                              </button>
+                              <div
+                                className={cx(
+                                  'list-item-description',
+                                  listItemDescriptionClassname
+                                    ? listItemDescriptionClassname
+                                    : '',
+                                  isExpandable
+                                    ? state.activeItem === key
+                                      ? 'show'
+                                      : 'hide'
+                                    : '',
+                                )}
+                                dangerouslySetInnerHTML={{
+                                  __html: DOMPurify.sanitize(value.description),
+                                }}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+                {providedDroppable.placeholder}
+              </>
+            )}
+          </Droppable>
+        </DragDropContext>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 };
 
 export default compose(
